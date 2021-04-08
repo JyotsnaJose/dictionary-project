5d9bab5394c348b1d34594343e8ff3b70bed2111
Jyotsna Jose <jyotsnamaria@gmail.com>
2021-04-08 16:52:58 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dictionary</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
